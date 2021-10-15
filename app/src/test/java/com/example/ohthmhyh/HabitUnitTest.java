77464219a78ff63b55a675acb05f3f2baafd2fd5
wiihoffmann <Wiihoffmann@yahoo.ca>
2021-10-14 20:27:30 -0600

    implement initial habit class (#41)
    Initial implementation of the habit class. Also wrote a quick test for the habit class and random habit generator method. Updated .gitignore to ignore some localization files. Modified the app build.gradle to allow for using the LocalDate object on API levels <26 because working with Calendar objects makes me want to cry.
    
    This should satisfy issue #26

---
+++ app/src/test/java/com/example/ohthmhyh/HabitUnitTest.java
@@ -0,0 +1,46 @@
+package com.example.ohthmhyh;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.EnumSet;
+
+
+public class HabitUnitTest {
+    @Test
+    public void Init_Habit_1() {
+        Habit h = new Habit();
+
+        // build the parameters
+        EnumSet<Habit.Days> sched = EnumSet.noneOf(Habit.Days.class);
+        sched.add(Habit.Days.Mon);
+        sched.add(Habit.Days.Wed);
+        sched.add(Habit.Days.Fri);
+
+        // set the parameters
+        h.setSchedule(sched);
+        h.setName("test name");
+        h.setDescription("test description");
+        h.setStartDate(LocalDate.now());
+
+        // test the parameters
+        assertEquals("test name", h.getName());
+        assertEquals("test description", h.getDescription());
+        assertEquals(sched, h.getSchedule());
+        assertEquals(LocalDate.now(), h.getStartDate());
+    }
+
+    @Test
+    public void Rand_Habit() {
+        Habit h = Habit.makeDummyHabit();
+
+        // look into the output log to make sure stuff was generated
+        System.out.println("NAME: " + h.getName());
+        System.out.println("DESC: " + h.getDescription());
+        System.out.println("SCHEDULE: " + h.getSchedule());
+        System.out.println("START DATE: " + h.getStartDate());
+        assert(true);
+    }
+
+}
