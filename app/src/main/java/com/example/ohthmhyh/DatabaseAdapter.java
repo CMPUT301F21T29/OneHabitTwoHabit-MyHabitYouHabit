578d7e055bebfddcc261c58d9afc1932575a86dc
wiihoffmann <Wiihoffmann@yahoo.ca>
2021-10-20 23:35:56 -0600

    Create DatabaseAdapter.java
    This is currently NOT functional (so please don't pull it), but is being pushed so that everyone can see the methods and structure of the class which will be responsible for pushing and pulling data from Firestore and the like.

---
+++ app/src/main/java/com/example/ohthmhyh/DatabaseAdapter.java
@@ -0,0 +1,40 @@
+package com.example.ohthmhyh;
+
+import java.util.ArrayList;
+
+public class DatabaseAdapter{
+
+    public void pushHabit(Habit habit){
+
+    }
+
+    public ArrayList<Habit> pullHabits(){
+
+        return new ArrayList<Habit>();
+    }
+
+    public void pushHabitEvent(Habit habit){
+
+    }
+
+    public ArrayList<HabitEvent> pullHabitEvents(){
+
+        return new ArrayList<HabitEvent>();
+    }
+
+    public void updateUser(User profile){
+
+    }
+
+    public User getUser(){
+
+        return null;
+    }
+
+    public ArrayList<User> getFollowing(){
+
+        return null;
+    }
+
+
+}
