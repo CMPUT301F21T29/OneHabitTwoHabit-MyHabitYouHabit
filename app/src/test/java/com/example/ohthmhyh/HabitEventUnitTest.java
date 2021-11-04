5b2d3f64cfdaa6230846653313b4a665f49aaa01
Matt <mwood2@ualberta.ca>
2021-11-04 17:05:59 -0600

    FINALLY have a working gradle file. All UI and Unit tests (junit4) now run. Using targetSDK=28

--- app/src/test/java/com/example/ohthmhyh/HabitEventUnitTest.java
+++ app/src/test/java/com/example/ohthmhyh/HabitEventUnitTest.java
@@ -1,17 +1,25 @@
 package com.example.ohthmhyh;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.runner.AndroidJUnit4;
+
 import static org.junit.Assert.*;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+@RunWith(AndroidJUnit4.class)
 public class HabitEventUnitTest {
-
+    private Context context = ApplicationProvider.getApplicationContext();
 
     @Test
     public void Init_Habit_1() {
         int h = 1;
-        //Bitmap b = BitmapFactory.decodeResource(solo.getCurrentActivity().getResources(),R.drawable.lol_pic);
+        Bitmap b = BitmapFactory.decodeResource(context.getResources(),R.drawable.lol_pic);
     }
 }
