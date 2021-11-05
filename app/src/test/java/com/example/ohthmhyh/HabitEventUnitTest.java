cd9d5d6f3e91fd285cf173ab2f44f1063b89e115
Matt <mwood2@ualberta.ca>
2021-11-04 22:48:14 -0600

    Prior code had a lot of unnecesary complexity. This fixes that.

---
+++ app/src/test/java/com/example/ohthmhyh/HabitEventUnitTest.java
@@ -0,0 +1,130 @@
+package com.example.ohthmhyh;
+
+//@author Matt
+
+//import static org.junit.Assert.*;
+//import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import android.graphics.Bitmap;
+import android.location.Address;
+
+//import androidx.test.runner.AndroidJUnit4;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
+
+/**
+ * This class is used to run tests on the HabitEvent class
+ */
+public class HabitEventUnitTest {
+
+    /**
+     * A simple function to create a sample habit, to use for testing
+     * @return sample habit
+     */
+    Habit createHabit() {
+        //Initialize habit for testing purposes
+        Habit h = new Habit();
+        h.setName("test name");
+        h.setDescription("test description");
+        h.setStartDate(LocalDate.now().toEpochDay());
+        h.setUHID(2021);
+        h.scheduleAddDay(Habit.Days.Mon);
+        h.scheduleAddDay(Habit.Days.Wed);
+        h.scheduleAddDay(Habit.Days.Wed);
+        h.scheduleAddDay(Habit.Days.Fri);
+
+        return h;
+    }
+
+    /**
+     * simple function to create a sample address, to use for testing
+     * @return sample address
+     */
+    Address createAddress() {
+        Locale l = new Locale("en", "US");
+        Address a = new Address(l);
+        return a;
+
+    }
+
+    /**
+     * Test building a habitevent with predefined values. Also tests getters.
+     * @throws Exception
+     */
+    @Test
+    public void testConstrAndGetters() {
+        Habit h = createHabit();
+        String comment = "Testing the comment";
+        Address a = createAddress();
+
+        Bitmap b = Bitmap.createBitmap(2, 3, Bitmap.Config.ARGB_8888); //create dummy bitmap
+
+        int flag = -1;
+
+        HabitEvent event = new  HabitEvent(h, comment, a, b, flag);
+
+        assertEquals(h, event.getHabit());
+        assertEquals(comment, event.getComment());
+        assertEquals(a, event.getLocatoion());
+        assertEquals(b, event.getBitmapPic());
+        assertEquals(flag, event.getFlag());
+    }
+
+    /**
+     * Test building a habitevent with predefined values. Then it changes those values with
+     * the setter methods, and verifies if they were set correctly using a getter method
+     * @throws Exception
+     */
+    @Test
+    public void testSetters() {
+        //initialize the habbit event
+        Habit h = createHabit();
+        String comment = "Testing the comment";
+        Address a = createAddress();
+
+        Bitmap b = Bitmap.createBitmap(2, 3, Bitmap.Config.ARGB_8888); //create dummy bitmap
+
+        int flag = -1;
+        HabitEvent event = new  HabitEvent(h, comment, a, b, flag);
+
+        //create alternate parameters
+        Habit h2 = new Habit();
+            h2.setName("test name");
+            h2.setDescription("test description");
+            h2.setStartDate(LocalDate.now().toEpochDay());
+            h2.setUHID(2021);
+            h2.scheduleAddDay(Habit.Days.Mon);
+            h2.scheduleAddDay(Habit.Days.Wed);
+            h2.scheduleAddDay(Habit.Days.Wed);
+            h2.scheduleAddDay(Habit.Days.Fri);
+        String comment2 = "New comment?";
+        Locale l = new Locale("fr", "US");
+        Address a2 = new Address(l);
+
+        //create dummy bitmap
+        Bitmap b2 = Bitmap.createBitmap(3, 4, Bitmap.Config.ARGB_8888);
+
+        int flag2 = 0;
+
+        //set everything to it's new values
+        event.setHabit(h2);
+        event.setComment(comment2);
+        event.setLocatoion(a2);
+        event.setBitmapPic(b2);
+        event.setFlag(flag2);
+
+        assertEquals(h2, event.getHabit());
+        assertEquals(comment2, event.getComment());
+        assertEquals(a2, event.getLocatoion());
+        assertEquals(b2, event.getBitmapPic());
+        assertEquals(flag2, event.getFlag());
+    }
+}
---
+++ app/src/test/java/com/example/ohthmhyh/UserTest.java
@@ -0,0 +1,100 @@
+package com.example.ohthmhyh;
+
+//@author Matt
+
+//import static org.junit.Assert.*;
+//import static org.junit.Assert.assertEquals;
+//import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+
+/**
+ * This class is used to run tests on the user class
+ */
+public class UserTest {
+
+
+    /**
+     * Test creating a user with an empty constructor. Also tests some getters and setters
+     * @throws Exception
+     */
+    @Test
+    public void testEmptyConstr() {
+        User u = new User();
+        String username = "hello";
+
+        //set values
+        u.setUsername(username);
+
+        //check that values were set correctly
+        assertEquals(u.getUsername(), username);
+    }
+
+    /**
+     * Test creating a user with a non empty constructor. Also tests some getters
+     * @throws Exception
+     */
+    @Test
+    public void testConstr() {
+        String username = "hello";
+        User u = new User(username);
+
+        //check that values were set correctly
+        assertEquals(u.getUsername(), username);
+    }
+
+
+    /**
+     * Test all getters and setters for the user class
+     * @throws Exception
+     */
+    @Test
+    public void testGettersSetters(){
+        //initialize values
+        String username = "suh dood";
+        String name = "jim halpert";
+        String bio = "I like cheese";
+        ArrayList<String> friendList = new ArrayList<>();
+        friendList.add("Joe");
+        friendList.add("Michael");
+        friendList.add("Fred");
+        friendList.add("Jimmy");
+        int UPIDCounter = 242332;
+
+        User u = new User(); //create user
+
+        //set values
+        u.setUsername(username);
+        u.setName(name);
+        u.setBio(bio);
+        u.setFriendList(friendList);
+        u.setUPIDCounter(UPIDCounter);
+
+        //make sure the newly set values are what they're supposed to be using the getters
+        assertEquals(username, u.getUsername());
+        assertEquals(name, u.getName());
+        assertEquals(bio, u.getBio());
+        assertEquals(friendList, u.getFriendList());
+        assertEquals(UPIDCounter, u.getUPIDCounter());
+    }
+
+
+    /**
+     * Test the nextUPID() method in the user class
+     * @throws Exception
+     */
+    @Test
+    public void testNextUPID(){
+        int UPIDCounter = 242332;
+        User u = new User(); //create user
+
+        u.setUPIDCounter(UPIDCounter);
+        assertEquals(UPIDCounter, u.getUPIDCounter());
+        assertEquals(UPIDCounter, u.nextUPID());
+    }
+}
+
