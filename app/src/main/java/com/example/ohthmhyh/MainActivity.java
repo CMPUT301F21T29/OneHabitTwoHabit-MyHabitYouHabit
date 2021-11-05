cda516252cbb4dd631e38fee502539e8af5f16b0
Cooperthedog2 <70604965+Cooperthedog2@users.noreply.github.com>
2021-11-04 20:24:59 -0600

    Missing import

--- app/src/main/java/com/example/ohthmhyh/MainActivity.java
+++ app/src/main/java/com/example/ohthmhyh/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ohthmhyh;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
