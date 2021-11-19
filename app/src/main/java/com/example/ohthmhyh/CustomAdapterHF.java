728fdc506dab195f0259d3aeff2c905b6af93b9b
Matt Wood <woodepic@gmail.com>
2021-11-18 20:21:41 -0700

    Made & Implemented a pretty habits list (#134)
    * Successfully added and tested a progress tracker for each habit
    
    * progress bar is now functional
    
    * implemented applicable days showing up in habit list
    
    * nothing works lol. Temporary save
    
    * Fixed calculation
    
    * Opportunity function complete, and tested to work flawlessly
    
    * addressed most of Chris's concerns
    
    * Finished getAdherence() and it's unit tests
    
    * Added misc.xml to the gitIgnore
    
    * changed a comment
    
    * removed unneccessary imports
    
    * changed a comment
    
    * removed commented-out code
    
    * Rename habit XML file
    
    Co-authored-by: Matt <mwood2@ualberta.ca>
    Co-authored-by: Ian C <insung1@ualberta.ca>
    Co-authored-by: Christian Jans <cjjans@ualberta.ca>

--- app/src/main/java/com/example/ohthmhyh/CustomAdapterHF.java
+++ app/src/main/java/com/example/ohthmhyh/CustomAdapterHF.java
@@ -2,11 +2,15 @@ package com.example.ohthmhyh;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,6 +20,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.ohthmhyh.database.HabitList;
+import com.example.ohthmhyh.entities.Habit;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 /**
  * A simple RecycleviewAdapter for the Habit list.
@@ -43,7 +51,7 @@ public class CustomAdapterHF extends RecyclerView.Adapter<CustomAdapterHF.Myview
     @NonNull
     @Override
     public Myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item_hf,parent,false);
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_habit,parent,false);
         Myviewholder holder = new Myviewholder(view, mOntouchListener);
 
         return holder;
@@ -56,6 +64,8 @@ public class CustomAdapterHF extends RecyclerView.Adapter<CustomAdapterHF.Myview
         //Need to error check because somethings might be null
         holder.name.setText(habitList.getHabit(position).getName());
         holder.description.setText(habitList.getHabit(position).getDescription());
+        setProgressBar(holder, position);
+        setDays(holder, position);
     }
 
     /**
@@ -99,6 +109,17 @@ public class CustomAdapterHF extends RecyclerView.Adapter<CustomAdapterHF.Myview
     {
         TextView name;
         TextView description;
+        ProgressBar pb;
+        TextView percent;
+
+        //days of week
+        TextView sun;
+        TextView mon;
+        TextView tues;
+        TextView wed;
+        TextView thurs;
+        TextView fri;
+        TextView sat;
 
         GestureDetector mGestureDetector;
         ConstraintLayout parentLayout;
@@ -107,6 +128,19 @@ public class CustomAdapterHF extends RecyclerView.Adapter<CustomAdapterHF.Myview
             super(itemView);
             name = itemView.findViewById(R.id.name_rv);
             description = itemView.findViewById(R.id.habit_description_rv);
+            pb = (ProgressBar) itemView.findViewById(R.id.pb);
+            percent = itemView.findViewById(R.id.percent);
+
+            //days of week
+            sun = itemView.findViewById(R.id.sun);
+            mon = itemView.findViewById(R.id.mon);
+            tues = itemView.findViewById(R.id.tues);
+            wed = itemView.findViewById(R.id.wed);
+            thurs = itemView.findViewById(R.id.thurs);
+            fri = itemView.findViewById(R.id.fri);
+            sat = itemView.findViewById(R.id.sat);
+
+
             //This is the name of the contrant layout in display HE list
             parentLayout = itemView.findViewById(R.id.rv_cl);
 
@@ -169,4 +203,115 @@ public class CustomAdapterHF extends RecyclerView.Adapter<CustomAdapterHF.Myview
          */
         void onItemClicked(int position);
     }
+
+    /**
+     * Contains the logic to set the progress bar, both in magnitude and colour
+     * Also set the % value
+     * @author Matt
+     * @param holder the viewholder holding objects
+     * @param position the position of the habit in the list that we are using
+     */
+    public void setProgressBar(@NonNull Myviewholder holder, @SuppressLint("RecyclerView") int position) {
+        double progress = habitList.getHabit(position).getAdherence(LocalDate.now());
+
+        //set colours of bar and text to grey
+        holder.percent.setTextColor(context.getResources().getColor(R.color.progressBarGray));
+        holder.pb.setProgressTintList(ColorStateList.valueOf(context.getResources().getColor(R.color.progressBarGray)));
+
+        int progressPercent = (int) progress;
+        holder.pb.setProgress(progressPercent);
+        holder.percent.setText(String.valueOf(progressPercent) + "%");
+
+        //set colour based on progress
+        if (progress == Constants.ADHERENCE_TEXT_GREEN_THRESHOLD) {
+            holder.percent.setTextColor(context.getResources().getColor(R.color.progressBarGreen));
+        }
+
+        if (progress >= Constants.ADHERENCE_PROGRESS_BAR_GREEN_THRESHOLD) {
+            //make progress bar green
+            holder.pb.setProgressTintList(ColorStateList.valueOf(context.getResources().getColor(R.color.progressBarGreen)));
+        }
+        else if (progress >= Constants.ADHERENCE_PROGRESS_BAR_AMBER_THRESHOLD) {
+            //make progress bar amber
+            holder.pb.setProgressTintList(ColorStateList.valueOf(context.getResources().getColor(R.color.progressBarAmber))); //amber
+
+        }
+        else if (progress > Constants.ADHERENCE_PROGRESS_BAR_RED_THRESHOLD) {
+            //make progress bar red
+            holder.pb.setProgressTintList(ColorStateList.valueOf(Color.RED));
+        }
+    }
+
+    /**
+     * contains the logic to bold and set text colour of applicable days in the week
+     * @author Matt
+     * @param holder the viewholder holding objects
+     * @param position the position of the habit in the list that we are using
+     */
+    public void setDays(@NonNull Myviewholder holder, @SuppressLint("RecyclerView") int position) {
+        //Bold/change colour of the days for which a habit is applicable
+
+        ArrayList<Habit.Days> days = habitList.getHabit(position).getSchedule();
+
+        float minOpacity = 0.3f;
+
+        //set all to default params
+        holder.sun.setTypeface(null, Typeface.NORMAL);
+        holder.mon.setTypeface(null, Typeface.NORMAL);
+        holder.tues.setTypeface(null, Typeface.NORMAL);
+        holder.wed.setTypeface(null, Typeface.NORMAL);
+        holder.thurs.setTypeface(null, Typeface.NORMAL);
+        holder.fri.setTypeface(null, Typeface.NORMAL);
+        holder.sat.setTypeface(null, Typeface.NORMAL);
+        holder.sun.setAlpha(minOpacity);
+        holder.mon.setAlpha(minOpacity);
+        holder.tues.setAlpha(minOpacity);
+        holder.wed.setAlpha(minOpacity);
+        holder.thurs.setAlpha(minOpacity);
+        holder.fri.setAlpha(minOpacity);
+        holder.sat.setAlpha(minOpacity);
+
+        if(days.contains(Habit.Days.Sun)){
+            // bold / change colour
+            holder.sun.setTypeface(null, Typeface.BOLD);
+            holder.sun.setAlpha(1f);
+        }
+
+        if(days.contains(Habit.Days.Mon)){
+            // bold / change colour
+            holder.mon.setTypeface(null, Typeface.BOLD);
+            holder.mon.setAlpha(1f);
+        }
+
+        if(days.contains(Habit.Days.Tue)){
+            // bold / change colour
+            holder.tues.setTypeface(null, Typeface.BOLD);
+            holder.tues.setAlpha(1f);
+        }
+
+        if(days.contains(Habit.Days.Wed)){
+            // bold / change colour
+            holder.wed.setTypeface(null, Typeface.BOLD);
+            holder.wed.setAlpha(1f);
+        }
+
+        if(days.contains(Habit.Days.Thu)){
+            // bold / change colour
+            holder.thurs.setTypeface(null, Typeface.BOLD);
+            holder.thurs.setAlpha(1f);
+        }
+
+        if(days.contains(Habit.Days.Fri)){
+            // bold / change colour
+            holder.fri.setTypeface(null, Typeface.BOLD);
+            holder.fri.setAlpha(1f);
+        }
+
+        if(days.contains(Habit.Days.Sat)){
+            // bold / change colour
+            holder.sat.setTypeface(null, Typeface.BOLD);
+            holder.sat.setAlpha(1f);
+        }
+    }
+
 }
