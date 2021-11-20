43c8e6b2117ca280c9c14859fb4a0cd447df640c
Christian Jans <cjjans@ualberta.ca>
2021-11-20 09:58:00 -0700

    Fix newline mistake

--- app/src/main/java/com/example/ohthmhyh/CustomAdapterHF.java
+++ app/src/main/java/com/example/ohthmhyh/CustomAdapterHF.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.ItemTouchHelper;
