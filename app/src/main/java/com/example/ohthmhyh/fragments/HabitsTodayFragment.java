fef8690a804f2a7b48adaff648be558fe368ef5f
Christian Jans <cjjans@ualberta.ca>
2021-11-21 09:38:31 -0700

    Update with changes from main

--- app/src/main/java/com/example/ohthmhyh/fragments/HabitsTodayFragment.java
+++ app/src/main/java/com/example/ohthmhyh/fragments/HabitsTodayFragment.java
@@ -3,7 +3,6 @@ package com.example.ohthmhyh.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,23 +10,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.ohthmhyh.CustomAdapterHTF;
+import com.example.ohthmhyh.adapters.HabitTodayRecyclerViewAdapter;
 import com.example.ohthmhyh.database.DatabaseAdapter;
 import com.example.ohthmhyh.entities.Habit;
 import com.example.ohthmhyh.database.HabitList;
 import com.example.ohthmhyh.R;
-import com.example.ohthmhyh.TouchingHandlingHTF;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HabitsTodayFragment extends Fragment implements CustomAdapterHTF.OntouchListener {
+public class HabitsTodayFragment extends Fragment {
 
     private HabitList habitList;
-    private CustomAdapterHTF adapter;
+    private HabitTodayRecyclerViewAdapter adapter;
     private DatabaseAdapter databaseAdapter;
 
     public HabitsTodayFragment(){/* Required empty public constructor*/}
@@ -46,49 +43,37 @@ public class HabitsTodayFragment extends Fragment implements CustomAdapterHTF.On
                              Bundle savedInstanceState) {
         // Create a new alert dialog when Add a Habit button is clicked
 
+        // Inflate the layout and get views for this fragment
         View view = inflater.inflate(R.layout.fragment_habits_today, container, false);
-
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view_htf);
 
         // Get the HabitList from the database.
-        databaseAdapter = new DatabaseAdapter();
+        databaseAdapter = DatabaseAdapter.getInstance();
         databaseAdapter.pullHabits(new DatabaseAdapter.HabitCallback() {
             @Override
             public void onHabitCallback(HabitList hList) {
-                habitList = hList;
-
                 //make smaller list of habits to put into the recycler view
-                ArrayList<Habit> newList = new ArrayList<>();
+                ArrayList<Habit> habitsToday = new ArrayList<>();
 
                 for (int i = 0; i < hList.size(); i++){
-                    Habit h = hList.getHabit(i);
-                    if (h.isDueToday()) {
-                        newList.add(h);
+                    Habit habit = hList.getHabit(i);
+                    if (habit.isDueToday()) {
+                        habitsToday.add(habit);
                     }
                 }
 
+                // Make the habitsToday into a HabitList for compatibility with the recyclerView adapter
+                HabitList habitList = new HabitList();
+                habitList.setHabitList(habitsToday);
+
+                adapter = new HabitTodayRecyclerViewAdapter(getContext(), habitList);
+                recyclerView.setAdapter(adapter);
                 recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
                 recyclerView.setHasFixedSize(true);
-
-                adapter = new CustomAdapterHTF(getContext(), HabitsTodayFragment.this, newList, habitList);
-                ItemTouchHelper.Callback callback = new TouchingHandlingHTF(adapter);
-                ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
-                adapter.setTouchhelper(itemTouchHelper);
-                itemTouchHelper.attachToRecyclerView(recyclerView);
-                recyclerView.setAdapter(adapter);
             }
         });
 
         return view;
-    }
-
-    /**
-     * Called when an item in the RecyclerView is clicked.
-     * @param position The position of the item that was clicked.
-     */
-    @Override
-    public void onItemClicked(int position) {
-        // We don't actually want to do anything when clicking a habit
     }
 
 }
