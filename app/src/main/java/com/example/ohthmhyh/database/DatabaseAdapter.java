75b603aea3632e93687a31354fff93c62ba52a7e
Christian Jans <cjjans@ualberta.ca>
2021-11-28 16:57:52 -0700

    Cleanup DatabaseAdapter.java

--- app/src/main/java/com/example/ohthmhyh/database/DatabaseAdapter.java
+++ app/src/main/java/com/example/ohthmhyh/database/DatabaseAdapter.java
@@ -26,6 +26,8 @@ import java.util.ArrayList;
  */
 public class DatabaseAdapter{
 
+    // region Interfaces
+
     /**
      * A callback used when retrieving the users profile from the database.
      */
@@ -68,14 +70,22 @@ public class DatabaseAdapter{
         void onHabitEventCallback(HabitEventList habitEvents);
     }
 
+    /**
+     * A general callback used to indicate that a call to the database has returned with the data.
+     */
     public interface OnLoadedListener {
         void onLoaded();
     }
 
+    /**
+     * A specific callback used to indicate that a call to the database has returned and a list of
+     * Habits (from the database) is ready to use.
+     */
     public interface OnLoadedHabitsListener {
         void onLoadedHabits(ArrayList<Habit> habits);
     }
 
+    // endregion
 
     private static DatabaseAdapter instance = null;
     private String UID;
@@ -84,12 +94,18 @@ public class DatabaseAdapter{
     private HabitEventList habitEventList = null;
     private User user = null;
 
-
+    /**
+     * The singleton constructor for the DatabaseAdapter.
+     */
     private DatabaseAdapter() {
         db = FirebaseFirestore.getInstance();
         UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
+    /**
+     * The getInstance method for the DatabaseAdapter singleton class.
+     * @return The instance of the DatabaseAdapter.
+     */
     public static DatabaseAdapter getInstance() {
         if (instance == null) {
             instance = new DatabaseAdapter();
@@ -110,7 +126,6 @@ public class DatabaseAdapter{
         }
         return false;
     }
-
 
     /**
      * Call this method to check if a username is in the database. Need to do this when changing a
@@ -136,7 +151,6 @@ public class DatabaseAdapter{
         });
     }
 
-
     /**
      * Call this method to get the UID associated with a username
      * @param username The username to get the UID for
@@ -159,7 +173,10 @@ public class DatabaseAdapter{
         });
     }
 
-
+    /**
+     * Pulls all the data for the user (the HabitList, HabitEventList, and the User objects).
+     * @param callback The callback used to indicate that this transaction is complete.
+     */
     public void pullAll(DatabaseAdapter.OnLoadedListener callback) {
         pullHabitEventList(new OnLoadedListener() {
             @Override
@@ -179,7 +196,11 @@ public class DatabaseAdapter{
         });
     }
 
-
+    /**
+     * Creates data (the HabitList, HabitEventList, and the User objects) for a user and pushes them
+     * to the database asynchronously.
+     * @param username The username to give this user.
+     */
     public void createDataForNewUser(String username) {
         UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
         habitList = new HabitList();
@@ -191,7 +212,10 @@ public class DatabaseAdapter{
         pushUser(user);
     }
 
-
+    /**
+     * Requests the HabitList from the database.
+     * @param callback The callback used to indicate when the transaction is complete.
+     */
     public void pullHabitList(DatabaseAdapter.OnLoadedListener callback) {
         DocumentReference documentReference = db.collection("Habits").document(UID);
         documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -204,6 +228,10 @@ public class DatabaseAdapter{
         });
     }
 
+    /**
+     * Requests the HabitList from the database for a specific user given their UID.
+     * @param callback The callback used to indicate when the transaction is complete.
+     */
     public void pullHabitsForUser(String userUID, DatabaseAdapter.OnLoadedHabitsListener callback) {
         DocumentReference documentReference = db.collection("Habits").document(userUID);
         documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -221,7 +249,10 @@ public class DatabaseAdapter{
         });
     }
 
-
+    /**
+     * Requests the HabitEventList from the database.
+     * @param callback The callback used to indicate when the transaction is complete.
+     */
     public void pullHabitEventList(DatabaseAdapter.OnLoadedListener callback) {
         DocumentReference documentReference = db.collection("HabitEvents").document(UID);
         documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -234,7 +265,10 @@ public class DatabaseAdapter{
         });
     }
 
-
+    /**
+     * Requests the User from the database.
+     * @param callback The callback used to indicate when the transaction is complete.
+     */
     public void pullUser(DatabaseAdapter.OnLoadedListener callback) {
         DocumentReference documentReference = db.collection("Profiles").document(UID);
         documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -247,18 +281,13 @@ public class DatabaseAdapter{
         });
     }
 
-
-
-    public User getUser() {
-        return user;
-    }
-
+    // region Habit Methods
 
     /**
-     * ----------------------- Habit methods. -----------------------
+     * Returns the Habit at a given index in the HabitList.
+     * @param index The index of the Habit.
+     * @return The Habit at the given index.
      */
-
-
     public Habit habitAtIndex(int index) {
         return habitList.getHabit(index);
     }
@@ -279,15 +308,27 @@ public class DatabaseAdapter{
         return -1;
     }
 
+    /**
+     * Returns the number of Habits that the current user has.
+     * @return The number of Habits that the current user has.
+     */
     public int numberOfHabits() {
         return habitList.size();
     }
 
+    /**
+     * Adds a Habit to the user's list of Habits.
+     * @param habit The Habit to add.
+     */
     public void addHabit(Habit habit) {
         habitList.addHabit(habit);
         pushHabits(habitList);
     }
 
+    /**
+     * Removes a Habit from the user's list of Habits.
+     * @param index The index of the Habit to remove.
+     */
     public void removeHabit(int index) {
         habitList.removeHabit(index);
         pushHabits(habitList);
@@ -303,67 +344,117 @@ public class DatabaseAdapter{
         pushHabits(habitList);
     }
 
+    /**
+     * Replaces a Habit at an index.
+     * @param index The index of the Habit to replace.
+     * @param habit The new Habit to place at this index.
+     */
     public void replaceHabit(int index, Habit habit) {
         habitList.replaceHabit(index, habit);
         pushHabits(habitList);
     }
 
+    /**
+     * Moves a Habit from one index to another.
+     * @param fromIndex The index of the Habit to move.
+     * @param toIndex The index of where the Habit should move to.
+     */
     public void moveHabit(int fromIndex, int toIndex) {
         habitList.moveHabit(fromIndex, toIndex);
         pushHabits(habitList);
     }
 
+    // endregion
+
+    // region Habit Event Methods
 
     /**
-     * ----------------------- HabitEvent methods. -----------------------
+     * Returns the HabitEvent at the given index.
+     * @param index The index of the HabitEvent.
+     * @return The HabitEvent at the given index.
      */
-
-
     public HabitEvent habitEventAtIndex(int index) {
         return habitEventList.getHabitEvent(index);
     }
 
+    /**
+     * Returns the number of HabitEvents this user has.
+     * @return The number of HabitEvents this user has.
+     */
     public int numberOfHabitEvents() {
         return habitEventList.size();
     }
 
+    /**
+     * Adds a HabitEvent to this user's list of HabitEvents.
+     * @param habitEvent The HabitEvent to add.
+     */
     public void addHabitEvent(HabitEvent habitEvent) {
         habitEventList.addHabitEvent(habitEvent);
         pushHabitEvents(habitEventList);
     }
 
+    /**
+     * Removes a HabitEvent from this user's list of HabitEvents.
+     * @param index The HabitEvent to remove.
+     */
     public void removeHabitEvent(int index) {
         habitEventList.removeHabitEvent(index);
         pushHabitEvents(habitEventList);
     }
 
+    /**
+     * Replaces a HabitEvent at a given index with a new HabitEvent.
+     * @param index The HabitEvent to replace.
+     * @param habitEvent The new HabitEvent.
+     */
     public void replaceHabitEvent(int index, HabitEvent habitEvent) {
         habitEventList.replaceHabitEvent(index, habitEvent);
         pushHabitEvents(habitEventList);
     }
 
+    /**
+     * Moves a HabitEvent from one index to another.
+     * @param fromIndex The index of the HabitEvent to move.
+     * @param toIndex The index of the destination of the HabitEvent.
+     */
     public void moveHabitEvent(int fromIndex, int toIndex) {
         habitEventList.moveHabitEvent(fromIndex, toIndex);
         pushHabitEvents(habitEventList);
     }
 
+    /**
+     * Returns the next UPID of the HabitEvents.
+     * @return The next UPID of the HabitEvents.
+     */
     public int nextHabitEventUPID() {
         return habitEventList.nextUPID();
     }
 
+    // endregion
+
+    // region User Methods
 
     /**
-     * ----------------------- User methods. -----------------------
+     * Returns the username of the current user.
+     * @return The username of the current user.
      */
-
     public String userUsername() {
         return user.getUsername();
     }
 
+    /**
+     * Returns the friends list of the current user.
+     * @return The friends list of the current user.
+     */
     public ArrayList<String> userFriendList() {
         return user.getFriendList();
     }
 
+    /**
+     * Returns the friend requests list of the current user.
+     * @return The friend requests list of the current user.
+     */
     public ArrayList<String> userFriendRequests() {
         return user.getFriendRequests();
     }
@@ -448,6 +539,7 @@ public class DatabaseAdapter{
         pushUser(user);
     }
 
+    // endregion
 
     /**
      * Call this method to get the username associated with a UID
@@ -466,7 +558,6 @@ public class DatabaseAdapter{
         });
     }
 
-
     /**
      * Call with a user profile to update it in the database. This should be called whenever a data
      * member is changed in a user profile.
@@ -475,7 +566,6 @@ public class DatabaseAdapter{
     public void pushUser(User user){
         pushUser(UID, user);
     }
-
 
     /**
      * Call with a user profile to update it in the database. This should be called whenever a data
@@ -495,7 +585,6 @@ public class DatabaseAdapter{
         db.collection(Constants.HABIT_COLLECTION_NAME).document(UID).set(habits);
     }
 
-
     /**
      * Call this method to push the users habit events into the database
      * @param events The habit events to push into the DB
@@ -504,7 +593,6 @@ public class DatabaseAdapter{
         db.collection(Constants.HABIT_EVENT_COLLECTION_NAME).document(UID).set(events);
     }
 
-
     /**
      * Call this when a user logs-on in order to retrieve their user profile from the database
      * @param callback A callback which will be used to update the user profile
@@ -512,7 +600,6 @@ public class DatabaseAdapter{
     public void pullUser(DatabaseAdapter.ProfileCallback callback){
         pullUser(UID, callback);
     }
-
 
     /**
      * Call this when a user logs-on in order to retrieve their user profile from the database
@@ -530,7 +617,6 @@ public class DatabaseAdapter{
         });
     }
 
-
     /**
      * Call this method to pull the users habits from the database
      * @param callback A callback which is called when the query completes
@@ -538,7 +624,6 @@ public class DatabaseAdapter{
     public void pullHabits(HabitCallback callback){
         pullHabits(UID, callback);
     }
-
 
     /**
      * Call this method to pull the users habits from the database
@@ -557,7 +642,6 @@ public class DatabaseAdapter{
         });
     }
 
-
     /**
      * Call this method to pull this users habit events from the database
      * @param callback A callback which is called when the query completes
@@ -565,7 +649,6 @@ public class DatabaseAdapter{
     public void pullHabitEvents(HabitEventCallback callback){
         pullHabitEvents(UID, callback);
     }
-
 
     /**
      * Call this method to pull another users habit events from the database.
